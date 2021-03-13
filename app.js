650df2d57e3403732b08248d08934dcbeb586fbc
rushikeshgadekar <rushikeshgadekar17399@gmail.com>
2021-03-13 19:05:00 +0530

    final commit

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const url = "mongodb://localhost/testDB";
-// const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const url = "mongodb://localhost/testDB";
+const app = express();
 
-// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-// const con = mongoose.connection;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const con = mongoose.connection;
 
-// con.on("open", function () {
-//   console.log("connected....");
-// });
-
-// app.use(express.json());
-
-// const userRouter = require("./routes/users");
-// app.use("/users", userRouter);
-
-// app.listen(9000, function () {
-//   console.log("server started...");
-// });
-
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://mongodb:<password>@cluster0.9vz9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+con.on("open", function () {
+  console.log("connected....");
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected to atlas");
-  // perform actions on the collection object
-  client.close();
+
+app.use(express.json());
+
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+
+app.listen(9000, function () {
+  console.log("server started...");
 });
+
+// const MongoClient = require("mongodb").MongoClient;
+// const uri =
+//   "mongodb+srv://mongodb:<password>@cluster0.9vz9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   console.log("connected to atlas");
+//   // perform actions on the collection object
+//   client.close();
+// });
